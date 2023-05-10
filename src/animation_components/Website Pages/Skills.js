d3dc4e470969813bca3208b5c7a443ee40cac4fc
Kelvin Chen <kelvgpd@gmail.com>
2023-05-10 01:23:43 -0400

    added functionnal skills components

--- src/animation_components/Website Pages/Skills.js
+++ src/animation_components/Website Pages/Skills.js
@@ -1,8 +1,22 @@
+import { useState } from "react";
+import SkillBox from "./SkillBox";
+
 const Skills = () => { 
+
+    const [categories, setCategory] = useState ([
+        {category: "Development"},
+        {category: "Design"},
+        {category: "Other"}
+    ])
 
     return(
         <div className = "Skills">
-            <p>Skills</p>
+            {categories.map((category) => (
+                <div className="skillCategory">
+                    <h3 className="categoryName">{category.category}</h3>
+                    <SkillBox category={category.category}/>
+                </div>
+            ))}    
         </div>
     );
 };
